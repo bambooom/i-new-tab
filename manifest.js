d47b0514e307b1c344aa0bee091a40e98a1d75fc
bamboo <nmz89825@gmail.com>
2023-12-15 00:08:53 +0800

    feat: change appname in manifest

--- manifest.js
+++ manifest.js
@@ -6,7 +6,7 @@ import packageJson from './package.json' assert { type: 'json' };
  */
 const manifest = {
   manifest_version: 3,
-  name: packageJson.name,
+  name: packageJson.appName,
   version: packageJson.version,
   description: packageJson.description,
   permissions: ['storage', 'sidePanel'],
